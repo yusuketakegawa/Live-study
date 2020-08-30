237445a4c1a47e0a200987a38ff04beb28913ea5
tatuya4451 <tatuya44511336@gmail.com>
2020-08-30 16:57:13 +0900

    【マークアップ】メッセージ大枠完成

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,9 @@
 
 $(document).on('turbolinks:load',function () {
   $('.sidenav').sidenav({
-    edge: 'right'
+    edge: 'right',
+    inDuration: 400,
+    outDuration: 400
   });
   $('.tap-target').tapTarget();
   $('select').formSelect();
