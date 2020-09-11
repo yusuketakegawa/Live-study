af6ae05f5538da53ea698152e824440c08f5e9d1
tatuya4451 <tatuya44511336@gmail.com>
2020-09-11 15:41:35 +0900

    フォームボタンのif分岐

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module LiveStudy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Asia/Tokyo'
     config.generators do |g|
       g.stylesheets false
