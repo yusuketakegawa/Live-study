09febf96d12dba7befbf1733ff0de248b00bcafe
tatuya4451 <tatuya44511336@gmail.com>
2020-09-11 13:30:27 +0900

    部屋作成の際の画像アップロードとマークアップの全体修正

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,12 +1,12 @@
 $(function(){
-  $('#img-file').change(function(){
-    var file = $('input[type="file"]').prop('files')[0];
+  $(".new__content__field").change(function () {
+    var file = $('input[type="file"]').prop("files")[0];
     var fileReader = new FileReader();
-    fileReader.onloadend = function(){
-      var src = fileReader.result
-      var html = `<img src="${src}" width="100" height="100">`
-            $(".prev-contents").before(html);
-    }
-     fileReader.readAsDataURL(file);
-  })
+    fileReader.onloadend = function () {
+      var src = fileReader.result;
+      var html = `<img src="${src}" width="100%" height="100%">`;
+      $(".prev-contents").replaceWith(html);
+    };
+    fileReader.readAsDataURL(file);
+  });
 })
