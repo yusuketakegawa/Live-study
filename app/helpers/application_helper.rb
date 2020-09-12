2a848353a6f72c46b04004517257fe7990b59d38
tatuya4451 <tatuya44511336@gmail.com>
2020-09-12 22:48:18 +0900

    【サーバーサイド】ログイン、新規登録モーダル化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
