0840d63ced39a443632617a8e7bb6bb6d4753390
tatuya4451 <tatuya44511336@gmail.com>
2020-09-13 08:51:51 +0900

    【マークアップ】ユーザー情報編集の整え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to mypage_user_path, notice: "情報を更新しました"
     else
       render :edit
     end
