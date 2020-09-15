30ebbf292848c2af3dce6c07d522321c3e235744
tatuya4451 <tatuya44511336@gmail.com>
2020-09-15 16:41:43 +0900

    【サーバーサイド】ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,10 @@ class User < ApplicationRecord
       notification.save if notification.valid?
     end
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com', nickname: 'ゲストユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root "studies#index"
   resources :studies do
     member do
