fbabca040da21b6b174c2ea251264ac032012c57
tatuya4451 <tatuya44511336@gmail.com>
2020-09-19 13:15:02 +0900

    デプロイ準備

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module LiveStudy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.i18n.default_locale = :ja
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Asia/Tokyo'
     config.action_dispatch.rescue_responses.merge!(
       "YourNewException" => :not_found
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe User, type: :model do
   it "ニックネームがなければ無効な状態であること" do
     user = FactoryBot.build(:user, nickname: nil)
     user.valid?
-    expect(user.errors[:nickname]).to include("can't be blank")
+    expect(user.errors[:nickname]).to include("を入力してください")
   end
 
   it "メールアドレスがなければ無効な状態であること" do
     user = FactoryBot.build(:user, email: nil)
     user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+    expect(user.errors[:email]).to include("を入力してください")
   end
   it "重複したメールアドレスなら無効な状態であること" do
     user = FactoryBot.create(:user, email: "aaron@example.com")
     user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    expect(user.errors[:email]).to include("はすでに存在します")
   end
   it "ニックネームを文字列として返すこと" do
     user = FactoryBot.build(:user, nickname: "john")
