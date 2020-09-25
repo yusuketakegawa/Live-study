2fc82a2ae5a2c9887ec1970f7133eeeb37d283e4
tatuya4451 <tatuya44511336@gmail.com>
2020-09-25 10:39:34 +0900

    デプロイ追加修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  root "studies#index"
+  root "top#index"
   resources :studies, except: [:index] do
     member do
       delete 'finish'
