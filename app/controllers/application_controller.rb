1cd4f7b03ebdaa8ca8eb1a6af9d078185f1e9d48
tatuya4451 <tatuya44511336@gmail.com>
2020-10-03 11:54:30 +0900

    registrationcontrollerの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+  def after_sign_up_path_for(_resource)
+    studies_path
+ end
 
   def after_sign_in_path_for(resource)
     studies_path
