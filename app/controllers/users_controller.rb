07a9b9410acb166c1a20546d0bc2cf5a563f4d9e
tatuya4451 <tatuya44511336@gmail.com>
2020-10-15 19:17:48 +0900

    reademeを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :move_to_index
   before_action :set_user, only: %i[show mypage follows followers]
-  before_action :check_guest, only: :update
+  # before_action :check_guest, only: :update
   before_action :study_room, only: %i[show mypage]
   def show
     # roomがcreateされた時にcurrent_userと相手側のユーザーをentriesテーブルに記録する必要があるのでwhereメソッドでユーザーを探す処理。
